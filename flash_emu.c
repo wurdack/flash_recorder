4df9cc6b033d5795ca898ca16ed667469983d884
Richard Wurdack <rwurdack@jawbone.com>
2015-04-20 09:22:32 -0700

    [emulator] Use defines instead of static const. Linux gcc appears not to like my use of static const.

--- flash_emu.c
+++ flash_emu.c
@@ -1,38 +1,41 @@
+/*
+    RAM-backed flash emulator. Used for testing the sflash_recorder library.
+*/
 #include "sys.h"
 #include "flash.h"
 
-/** Block size for emulated flash */
-static uint32_t const c_flash_block_size = 4 * 1024;
+/** 4k block size for emulated flash */
+#define C_FLASH_BLOCK_SIZE      (4*1024)
 
 /** 8MB flash array */
-static uint32_t const c_flash_array_size = c_flash_block_size * 2048;
+#define C_FLASH_ARRAY_SIZE      (C_FLASH_BLOCK_SIZE * 2048)
 
 /** Erase value for bytes in flash */
-static uint8_t const c_flash_erase_value = 0xff;
+#define C_FLASH_ERASE_VALUE     (0xff)
 
 /** Memory that simulates flash storage */
-static uint8_t s_sflash_array[c_flash_array_size];
+static uint8_t s_sflash_array[C_FLASH_ARRAY_SIZE];
 
 /** Erase wear counter */
-static uint32_t s_sflash_wear[c_flash_array_size / c_flash_block_size];
+static uint32_t s_sflash_wear[C_FLASH_ARRAY_SIZE / C_FLASH_BLOCK_SIZE];
 
 /** Returns true if the address is on a block boundary */
 static bool sflash_is_aligned(uint32_t address)
 {
-    return (address & (c_flash_block_size - 1)) == 0;
+    return (address & (C_FLASH_BLOCK_SIZE - 1)) == 0;
 }
 
 /** Returns the base address of the block containing 'address' */
 static uint32_t sflash_align(uint32_t address)
 {
-    return (address & ~(c_flash_block_size - 1));
+    return (address & ~(C_FLASH_BLOCK_SIZE - 1));
 }
 
 /** Returns the block index of the block containing 'address' */
 static uint32_t sflash_block_from_address(uint32_t address)
 {
-    ASSERT(address < c_flash_array_size);
-    return address / c_flash_block_size;
+    ASSERT(address < C_FLASH_ARRAY_SIZE);
+    return address / C_FLASH_BLOCK_SIZE;
 }
 
 /**
@@ -41,7 +44,7 @@ static uint32_t sflash_block_from_address(uint32_t address)
 
 int sflash_init(void)
 {
-    memset(s_sflash_array, c_flash_erase_value, sizeof(s_sflash_array));
+    memset(s_sflash_array, C_FLASH_ERASE_VALUE, sizeof(s_sflash_array));
     memset(s_sflash_wear, 0, sizeof(s_sflash_wear));
     return 0;
 }
@@ -51,8 +54,8 @@ int sflash_geometry_get(uint32_t *block_size, uint32_t *array_size)
     ASSERT(block_size);
     ASSERT(array_size);
 
-    *block_size = c_flash_block_size;
-    *array_size = c_flash_array_size;
+    *block_size = C_FLASH_BLOCK_SIZE;
+    *array_size = C_FLASH_ARRAY_SIZE;
     return 0;
 }
 
@@ -60,14 +63,14 @@ int sflash_block_erase(uint32_t address)
 {
     ASSERT(sflash_is_aligned(address));
     address = sflash_align(address);
-    memset(s_sflash_array + address, c_flash_erase_value, c_flash_block_size);
+    memset(s_sflash_array + address, C_FLASH_ERASE_VALUE, C_FLASH_BLOCK_SIZE);
     ++s_sflash_wear[sflash_block_from_address(address)];
     return 0;
 }
 
 int sflash_write(uint32_t address, void const *buffer, uint32_t size)
 {
-    ASSERT(address + size <= c_flash_array_size);
+    ASSERT(address + size <= C_FLASH_ARRAY_SIZE);
 
     // To simulate how flash behaves, AND each byte into memory. This will
     // prevent a write from turning a 0 bit into a 1 bit.
@@ -86,7 +89,7 @@ int sflash_write(uint32_t address, void const *buffer, uint32_t size)
 
 int sflash_read(uint32_t address, void *buffer, uint32_t size)
 {
-    ASSERT(address + size <= c_flash_array_size);
+    ASSERT(address + size <= C_FLASH_ARRAY_SIZE);
     memcpy(buffer, s_sflash_array + address, size);
     return 0;
 }
